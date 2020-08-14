0107f942032095d59bbb1f92453759a17be4c2ed
f4nke <cool-vlad@mail.ru>
2020-08-14 18:03:55 +0300

    исправление ошибок

--- src/RomanNumbers.java
+++ src/RomanNumbers.java
@@ -93,7 +93,6 @@ public enum RomanNumbers {
             switch (oper) {
                 case '+':
                     out = value1 + value2;
-                    ;
                     break;
                 case '-':
                     out = value1 - value2;
